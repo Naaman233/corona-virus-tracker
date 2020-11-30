7ed8fff1b0f63de225939f844c423f2264dfeb4a
Makafui Naaman <makafuinaaman@Makafuis-MacBook-Pro.local>
2020-11-30 04:28:26 -0500

    resolved test problem

--- src/api/test/app.test.js
+++ src/api/test/app.test.js
@@ -9,11 +9,3 @@ it('renders without crashing', () => {
 })
 
 
-const cp = require("child_process");
-const verify = () => cp.exec("npm ls", error =>{
-    if(error){
-        console.error("Dependency mismatch between package.json and lock. Run: npm install");
-        throw error;
-    }
-    console.log("Dependencies verified =)");
-});
